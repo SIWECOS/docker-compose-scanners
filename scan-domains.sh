d2a86f38bdceeec0f2a3e938d29ed22f541560cb
Stephan Hradek <github.mails.skeeve@xoxy.net>
2019-09-26 12:46:45 +0200

    change array to case

--- scan-domains.sh
+++ scan-domains.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
-
-declare -A SCANNER_URL
-SCANNER_URL[ALL]=http://localhost:8000/api/v2/scan
-SCANNER_URL[CORE]=http://localhost:8000/api/v2/scan
-SCANNER_URL[INI_S]=http://localhost:8090/api/v1/check
-SCANNER_URL[DOMXSS]=http://localhost:8091/api/v1/domxss
-SCANNER_URL[HEADER]=http://localhost:8091/api/v1/header
-SCANNER_URL[INFOLEAK]=http://localhost:8093/api/v2/start
-SCANNER_URL[PORT]=http://localhost:8094/start
-SCANNER_URL[TLS]=http://localhost:8095/tls
+case "$1" in
+ALL)      url=http://localhost:8000/api/v2/scan ;;
+CORE)     url=http://localhost:8000/api/v2/scan ;;
+INI_S)    url=http://localhost:8090/api/v1/check ;;
+DOMXSS)   url=http://localhost:8091/api/v1/domxss ;;
+HEADER)   url=http://localhost:8091/api/v1/header ;;
+INFOLEAK) url=http://localhost:8093/api/v2/start ;;
+PORT)     url=http://localhost:8094/start ;;
+TLS)      url=http://localhost:8095/tls ;;
 # SCANNER_URL[MAIL]=
-SCANNER_URL[VERSION]=http://localhost:8096/api/v1/version
-
-
+VERSION)  url=http://localhost:8096/api/v1/version ;;
+*)        echo "Don't know about $1" ;;
+esac
 
 while read domain; do
 curl -X POST \
-  ${SCANNER_URL[$1]} \
+  ${url} \
   -H 'Content-Type: application/json' \
   -d '{
   "url": "'$domain'",
